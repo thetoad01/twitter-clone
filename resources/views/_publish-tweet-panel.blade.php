10e979339cb68e5918c8208facfa23099052bd9e
David Defoe <david@defoenet.com>
2020-03-24 15:36:09 -0400

    Initial interface setup

---
+++ resources/views/_publish-tweet-panel.blade.php
@@ -0,0 +1,19 @@
+<div class="border border-blue-400 rounded-lg py-6 px-6 mb-8">
+    <form action="/tweets" method="POST">
+        @csrf
+
+        <textarea name="body" id="body" class="w-full" placeholder="What's the directive?"></textarea>
+
+        <hr class="my-4">
+
+        <footer class="flex justify-between">
+            <img src="{{ auth()->user()->avitar }}" alt="" class="rounded-full mr-2">
+
+            <button type="submit" class="bg-blue-500 rounded-lg shadow p-2 text-white">Make it so!</button>
+        </footer>
+    </form>
+
+    @error('body')
+        <p class="text-red-500 text-sm mt-2">{{ $message }}</p>
+    @enderror
+</div>
