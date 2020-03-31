8a4facca9532d377bffa98300c8d9ce447830760
David Defoe <david@defoenet.com>
2020-03-31 12:41:20 -0400

    Layout using blade components

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
 Route::middleware('auth')->group(function() {
     Route::get('/tweets', 'TweetController@index')->name('home');
     Route::post('/tweets', 'TweetController@store')->name('tweets.store');
 });
 
 Route::get('/profiles/{user}', 'ProfilesController@show')->name('profiles.show');
-
-Auth::routes();
