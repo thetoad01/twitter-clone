c29ee43aef98d0b01c6e1d20e540eeffe5dea6d7
David Defoe <david@defoenet.com>
2020-04-14 15:10:52 -0400

    tweet like/dislike buttons w/count

---
+++ app/Likeable.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App;
+
+trait Likeable
+{
+    public function scopeWithLikes($query)
+    {
+        $query->leftJoinSub(
+            'select tweet_id, sum(liked) likes, sum(!liked) dislikes from likes group by tweet_id',
+            'likes',
+            'likes.tweet_id',
+            'tweets.id'
+        );
+    }
+
+    public function like($user = null, $liked = true)
+    {
+        return $this->likes()->updateOrCreate(
+            [
+                'user_id' => $user ? $user->id : auth()->id()
+            ],
+            [
+                'liked' => $liked
+            ]
+        );
+    }
+
+    public function dislike($user = null)
+    {
+        return $this->like($user, false);
+    }
+
+    public function isLikedBy(User $user)
+    {
+        return (bool) $user->likes
+            ->where('tweet_id', $this->id)
+            ->where('liked', true)
+            ->count();
+    }
+
+    public function isDislikedBy(User $user)
+    {
+        return (bool) $user->likes
+            ->where('tweet_id', $this->id)
+            ->where('liked', false)
+            ->count();
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
+}
