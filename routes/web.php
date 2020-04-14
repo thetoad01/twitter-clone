afb39654094bbb68fdde86cca94f64636e92c7c2
David Defoe <david@defoenet.com>
2020-04-14 17:37:24 -0400

    Like blade component

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/tweets', 'TweetsController@index')->name('home');
     Route::post('/tweets', 'TweetsController@store')->name('tweets.store');
 
+    Route::post('/tweets/{tweet}/like', 'TweetLikeController@store')->name('likes.store');
+    Route::delete('/tweets/{tweet}/like', 'TweetLikeController@destroy')->name('likes.destroy');
+
     Route::post('/profiles/{user:username}/follow', 'FollowsController@store');
     Route::get('/profiles/{user:username}/edit', 'ProfilesController@edit')->middleware('can:edit,user');
     Route::patch('/profiles/{user:username}', 'ProfilesController@update')->name('profile.update')->middleware('can:edit,user');
