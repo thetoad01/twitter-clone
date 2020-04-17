db130635ff21bff4717f186776882dfc22963914
David Defoe <david@defoenet.com>
2020-04-17 15:39:26 -0400

    Like button changes

--- resources/views/components/like-buttons.blade.php
+++ resources/views/components/like-buttons.blade.php
@@ -1,8 +1,8 @@
 <div class="d-flex">
     <form action="/tweets/{{ $tweet->id }}/like" method="POST">
         @csrf
-        <div class="d-flex align-middle mr-4">
-            <button type="submit" class="btn btn-link small">
+        <div class="d-flex align-middle">
+            <button type="submit" class="btn btn-link text-decoration-none small" title="like">
                 <i class="fa fa-thumbs-up {{ $tweet->isLikedBy(current_user()) ? 'text-success' : 'text-secondary' }}"></i>
                 {{ $tweet->likes ?: 0 }}
             </button>
@@ -13,7 +13,7 @@
         @csrf
         @method('delete')
         <div class="d-flex align-middle">
-            <button type="submit" class="btn btn-link small">
+            <button type="submit" class="btn btn-link text-decoration-none small" title="dislike">
                 <i class="fa fa-thumbs-down {{ $tweet->isDislikedBy(current_user()) ? 'text-danger' : 'text-secondary' }}"></i>
                 {{ $tweet->dislikes ?: 0 }}
             </button>
